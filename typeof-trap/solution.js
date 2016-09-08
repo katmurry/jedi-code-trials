193c94b5764801b1bae497efaf85637146debb21
katmurry <katmurry@gmail.com>
2016-09-07 23:33:32 -0700

    +solution.js: corrected missing semicolon for typeof-trap solution.

--- typeof-trap/solution.js
+++ typeof-trap/solution.js
@@ -1,11 +1,11 @@
-//a. A potential pitfall is that null is also considered as an object type. So if... 
+//a. A potential pitfall is that null is also considered as an object type. So if...
 
 var bar = null;
 
 //and you do
 
   console.log(typeof bar); //will return 'object';
-  console.log(typeof bar === 'object' ) //this returns true;
+  console.log(typeof bar === 'object' ); //this returns true;
 
 //b. A way around this is to check if that item is equal to null first -not equal in value and type, just value- then do your typeof check. Like this...
 
